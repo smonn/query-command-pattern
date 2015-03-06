12d7958a8eedda08f5906c5f1c5e6399f0aaf969
Simon Ingeson <simon.ingeson@ef.com>
2015-03-06 23:28:54 +0100

    fixing utilities namespace

--- Utilities/EnumerationClass.cs
+++ Utilities/EnumerationClass.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
-namespace Utilities
+namespace SampleEntityFramework.Utilities
 {
     public abstract class Enumeration : IComparable
     {
