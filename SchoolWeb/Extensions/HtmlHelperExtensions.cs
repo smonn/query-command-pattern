c3a13bb580f4267d6101173d56b313f185eb6d41
Simon Ingeson <simon.ingeson@ef.com>
2015-03-07 13:22:51 +0100

    removed paging duplication

---
+++ SchoolWeb/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using SampleEntityFramework.DomainModels.Common;
+using SampleEntityFramework.SchoolWeb.Models;
+
+namespace SampleEntityFramework.SchoolWeb.Extensions
+{
+    public static class HtmlHelperExtensions
+    {
+        public static MvcHtmlString Pagination<TModel>(this HtmlHelper html, PaginatedModel<TModel> model, string action,
+            string controller, string area = null)
+        {
+            return html.Partial("_Pagination", new PaginationViewModel<TModel>
+            {
+                Action = action,
+                Area = area,
+                Controller = controller,
+                Data = model,
+            });
+        }
+    }
+}
